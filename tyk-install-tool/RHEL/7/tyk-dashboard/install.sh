6b70ad3947f7194cacc104543a8ae73e2b3b1d0a
Zaid Albirawi <zaid@tyk.io>
2021-01-07 14:32:23 -0500

    Fix dashboard config

--- tyk-install-tool/RHEL/7/tyk-dashboard/install.sh
+++ tyk-install-tool/RHEL/7/tyk-dashboard/install.sh
@@ -22,9 +22,9 @@ sudo /opt/tyk-dashboard/install/setup.sh \
   --listenport=3000 \
   --redishost=localhost \
   --redisport=6379 \
-  --domain=http://ec2-44-234-121-209.us-west-2.compute.amazonaws.com \
+  --domain=ec2-44-234-121-209.us-west-2.compute.amazonaws.com \
   --mongo=mongodb://localhost:27017/tyk_analytics \
-  --tyk_api_hostname=http://ec2-44-234-121-209.us-west-2.compute.amazonaws.com:8080 \
+  --tyk_api_hostname=ec2-44-234-121-209.us-west-2.compute.amazonaws.com:8080 \
   --tyk_node_port=8080 \
   --portal_root=/portal
 
